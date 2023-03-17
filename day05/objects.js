c190136086498f9bdebf5986da3cb2d1a69519d2
Your Name <you@example.com>
2023-03-17 17:10:12 +0530

    feat:day05

---
+++ day05/objects.js
@@ -0,0 +1,17 @@
+const institute="Azhar acedemy"
+const mystery="answer"
+const obj={
+    a:3,
+    b:4,
+    foo(){},
+    xyz:()=>{},
+    ["first name"]:"sharad",
+    institute,
+    ["lname"]:"thenge",
+    [mystery]: 65,
+};
+console.log(obj["first name"]);
+console.log(obj.mystery);
+console.log(obj.answer);
+console.log(obj.lname);
+console.log(obj.institute);
