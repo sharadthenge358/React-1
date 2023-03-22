1915b847b121b448780cc9f96a244ad73abbc6c5
Your Name <you@example.com>
2023-03-22 22:40:11 +0530

    feat:day07

---
+++ day04/regular-vs-arrow-function.js
@@ -0,0 +1,3 @@
+// regular function gives acces to the calling function
+// arrow function gives access to their defining information
+// check the console of html in day 26 (function-arrow function)
