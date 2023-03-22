b8fb457e5854b05ed7dbf6d8bb4cbadf1cd3d69f
Your Name <you@example.com>
2023-03-22 22:49:12 +0530

    feat:day7

--- closure.js
+++
@@ -1,9 +0,0 @@
-function init(){
-    var name="mozilla";// the name is a local variable created by init
-    function displayName(){
-        // display name () is the inner function, that forms the closure
-        console.log(name);// use variable declared in the parent function
-    }
-    displayName();
-}
-init();
--- day4/regular-vs-arrow-function.js
+++
@@ -1,3 +0,0 @@
-// regular function gives acces to the calling function
-// arrow function gives access to their defining information
-// check the console of html in day 26 (function-arrow function)
